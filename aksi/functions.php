c2f706f1875e5691c4891f87ed635b46ab0c8e86
BertoPurba <bertojdoyvanpurba@gmail.com>
2024-12-12 14:35:32 +0700

    tes

--- aksi/functions.php
+++ aksi/functions.php
@@ -9,8 +9,10 @@ function query($query) {
 	global $conn;
 	$result = mysqli_query($conn, $query);
 	$rows = [];
-	while ( $row = mysqli_fetch_assoc($result) ) {
-		$rows[] = $row;
+	while ( $row = mysqli_fetch_assoc($result)
+	 )
+	  {
+		$rows[] = $row ;
 	}
 	return $rows;
 }
