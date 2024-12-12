77f5186a87c74d092c5dc37fda98ffae8188f464
BertoPurba <bertojdoyvanpurba@gmail.com>
2024-12-12 16:56:45 +0700

    change koneksi

--- aksi/koneksi.php
+++ aksi/koneksi.php
@@ -1,8 +1,8 @@
 <?php 
 	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$db = "pos-bengkel-seniman-koding";
+	$username = "tamj8198_tas";
+	$password = "MMt0123!#";
+	$db = "tamj8198_appbengkel";
 
 	$conn = new mysqli($servername, $username, $password, $db);
 ?>
